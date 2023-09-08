184800a379e6d21f87faa583819b7a85b8140f09
GitFeels <95645429+GitFeels@users.noreply.github.com>
2023-09-08 15:20:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// Retrieve elements from the HTML
+
 const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-// Get the current day of the week
+
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const currentDayIndex = new Date().getDay();
 const currentDayOfWeek = daysOfWeek[currentDayIndex];
 
-// Display the current day of the week
+
 currentDayElement.textContent = currentDayOfWeek;
     
-// Get the current UTC time in milliseconds
+
 const currentUTCTime = Date.now();
 
-// Display the current UTC time
+
 currentUTCTimeElement.textContent = currentUTCTime;
+
+function updateTime() {
+  const now = new Date();
+  const timeElement = document.getElementById('currentUTCTime');
+  timeElement.innerText = 'Current UTC Time: ' + now.toUTCString();
+}
+
+setInterval(updateTime, 1000);
