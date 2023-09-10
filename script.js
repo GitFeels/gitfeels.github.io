115fe82233b014c86fb62cdfbcd19e9b394cb257
GitFeels <95645429+GitFeels@users.noreply.github.com>
2023-09-10 23:23:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fri
 const currentDayIndex = new Date().getDay();
 const currentDayOfWeek = daysOfWeek[currentDayIndex];
 
-
 currentDayElement.textContent = currentDayOfWeek;
     
 
 const currentUTCTime = Date.now();
 
-
 currentUTCTimeElement.textContent = currentUTCTime;
+
 
 function updateTime() {
   const now = new Date();
   const timeElement = document.getElementById('currentUTCTime');
-  timeElement.innerText = 'Current UTC Time: ' + now.toUTCString();
+
+  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+  
+  let milliseconds = String(now.getUTCMilliseconds());
+ 
+  while (milliseconds.length < 3) {
+    milliseconds = '0' + milliseconds;
+  }
+  
+  const timeString = hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+  
+  timeElement.innerText = timeString;
 }
 
-setInterval(updateTime, 1000);
+setInterval(updateTime, 1);
